aaeaa1568bc828e7117c353835387caf453092de
Ivan Dashkevich <ivandashk.public@gmail.com>
2021-05-11 00:05:35 +0300

    router fix

--- src/index.js
+++ src/index.js
@@ -18,17 +18,15 @@ const store = configureAppStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <>
-        <Switch>
-          {examplesConfig.map(({ slug, code, rootSaga } ) => (
-            <Route path={`/${slug}`} key={slug}>
-              <MonitorInitializer sagaMonitor={sagaMonitor} sagaMiddleware={sagaMiddleware} rootSaga={rootSaga} code={code} />
-            </Route>
-          ))}
-          <Redirect from="/" to={`/${examplesConfig[0].slug}`} />
-        </Switch>
-      </>
+    <Router basename={'/saga-showcase'}>
+      <Switch>
+        {examplesConfig.map(({ slug, code, rootSaga } ) => (
+          <Route path={`/${slug}`} key={slug}>
+            <MonitorInitializer sagaMonitor={sagaMonitor} sagaMiddleware={sagaMiddleware} rootSaga={rootSaga} code={code} />
+          </Route>
+        ))}
+        <Redirect from="/" to={`/${examplesConfig[0].slug}`} />
+      </Switch>
     </Router>
   </Provider>,
   document.getElementById('root')
